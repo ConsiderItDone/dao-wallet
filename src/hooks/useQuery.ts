0b5060d84a5b466588378fc0d7d83ad4c31cf602
Mike <mikelrap@gmail.com>
2022-09-01 19:25:09 +0300

    integration

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,18 +1,10 @@
-import path from "path";
 import { usePolywrapInvoke } from "@polywrap/react";
 import { UsePolywrapInvoke } from "@polywrap/react/build/invoke";
 import { InvokeResult } from "@polywrap/core-js";
 
-const basePath = `${__dirname}/cases/wrappers`;
-
-const wrapperPath = path.resolve(`${basePath}/wasm-as/simple-storage`);
-
-const fsPath = `${wrapperPath}/build`;
-
-const fsUri = `fs/${fsPath}`;
 const ipfsUri = "wrap://ipfs/QmW6rsaFCx9mNDdqzKfGjmKv5bqtUknrd9a8RGiKisk5Xh";
 
-export const apiUri = fsUri || ipfsUri;
+export const apiUri = ipfsUri;
 
 export const useQuery = <TData = Record<string, unknown>>(
   method: string
