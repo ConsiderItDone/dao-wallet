f6abfd781620ad765797a94d3a2a857eddb63e69
koshelevandrey <tikr22@gmail.com>
2022-12-09 22:53:06 +0300

    updated connect accounts to website page

--- src/components/connectAccountsPage/index.tsx
+++ src/components/connectAccountsPage/index.tsx
@@ -6,6 +6,7 @@ import iconsObj from "../../assets/icons";
 import Icon from "../icon";
 import { ClipLoader } from "react-spinners";
 import { getImplicitAccountId } from "../../utils/account";
+import { ReactComponent as ArrowIcon } from "../../images/arrow.svg";
 
 const appLocalStorage = new LocalStorage();
 
@@ -26,7 +27,8 @@ export const ConnectAccountsPage = ({ website }: Props) => {
     ConnectAccountOption[] | undefined
   >(undefined);
 
-  const [isConfirming, setIsConfirming] = useState<boolean>(false);
+  const [isConnecting, setIsConnecting] = useState<boolean>(false);
+  const [tabIndex, setTabIndex] = useState<1 | 2>(1);
 
   useEffect(() => {
     const getAlreadyConnectedAccounts = async (website: string) => {
@@ -56,8 +58,12 @@ export const ConnectAccountsPage = ({ website }: Props) => {
     }
   }, [accounts, website]);
 
-  const onConfirm = async () => {
-    setIsConfirming(true);
+  const handleNext = () => {
+    setTabIndex(2);
+  };
+
+  const handleConnectAccounts = async () => {
+    setIsConnecting(true);
 
     await appLocalStorage.setWebsiteConnectedAccounts(
       website,
@@ -79,50 +85,167 @@ export const ConnectAccountsPage = ({ website }: Props) => {
     window.close();
   };
 
+  const goBack = () => {
+    setTabIndex(1);
+  };
+
+  const updateConnectedAccount = (
+    accountOption: ConnectAccountOption,
+    index: number
+  ) => {
+    setConnectAccountOptions((prevState) =>
+      prevState
+        ? [
+            ...prevState.slice(0, index),
+            { ...accountOption, isChosen: !accountOption.isChosen },
+            ...prevState.slice(index + 1),
+          ]
+        : []
+    );
+  };
+
+  const onSelectAll = () => {
+    const areAllSelected = connectAccountOptions?.every(
+      (account) => account.isChosen
+    );
+    const newIsChosenValue = !areAllSelected;
+    setConnectAccountOptions((prevState) => {
+      return prevState?.map((account) => ({
+        ...account,
+        isChosen: newIsChosenValue,
+      }));
+    });
+  };
+
+  const isAnyAccountChosen = connectAccountOptions?.some(
+    (account) => account.isChosen
+  );
+
   return (
     <div className="connectAccountsPageContainer">
-      <div className="body">
-        <div className="title">Choose accounts for connection with</div>
-        <div className="secondaryTitle">{website}</div>
-        <div className="accountsContainer">
-          {connectAccountOptions?.map((accountOption, index) => (
-            <div
-              className={`account ${
-                accountOption.isChosen ? "chosenAccount" : ""
-              }`}
-              key={index}
-              onClick={() => {
-                setConnectAccountOptions([
-                  ...connectAccountOptions.slice(0, index),
-                  { ...accountOption, isChosen: !accountOption.isChosen },
-                  ...connectAccountOptions.slice(index + 1),
-                ]);
-              }}
-            >
-              <div className="accountId">{accountOption.accountId}</div>
-              {accountOption.isChosen ? (
-                <Icon src={iconsObj.success} className="isChosenIcon" />
-              ) : null}
-            </div>
-          ))}
+      <header>
+        <div className="tabSelector">
+          <button onClick={goBack} className="backBtn" disabled={tabIndex <= 1}>
+            <ArrowIcon />
+          </button>
+          <div className="tabInfo">{tabIndex} of 2</div>
         </div>
+        <div className="brandName">DAO Wallet</div>
+      </header>
+      <div className="body">
+        <div className="websiteWrapper">
+          <img
+            src={`https://s2.googleusercontent.com/s2/favicons?domain=${website}`}
+            alt=""
+            className="favicon"
+          />
+          <div className="website">{website}</div>
+        </div>
+        <div className="title">
+          {tabIndex === 1 ? (
+            <>
+              <span>
+                Connect with <br /> DAO Wallet
+              </span>
+            </>
+          ) : (
+            <>
+              <span>Connect To</span>
+              <Icon className="arrowDown" src={iconsObj.arrowDownGroup} />
+            </>
+          )}
+        </div>
+        {tabIndex === 1 ? (
+          <div className="subtitle">
+            Select the account(s) to use on this site
+          </div>
+        ) : null}
+        {tabIndex === 1 ? (
+          <div className="connectAccountCheckboxWrapper selectAll">
+            <input
+              type="checkbox"
+              className="connectAccountCheckbox"
+              id="selectAll"
+              name="selectAll"
+              checked={connectAccountOptions?.every(
+                (account) => account.isChosen
+              )}
+              onClick={onSelectAll}
+            />
+            <label htmlFor="selectAll">Select all</label>
+          </div>
+        ) : null}
+        <div className="accountsContainer">
+          {tabIndex === 1
+            ? connectAccountOptions?.map((accountOption, index) => (
+                <div className="connectAccountCheckboxWrapper" key={index}>
+                  <input
+                    type="checkbox"
+                    className="connectAccountCheckbox"
+                    id={`account#${index}`}
+                    name={`account#${index}`}
+                    checked={accountOption.isChosen}
+                    onClick={() => updateConnectedAccount(accountOption, index)}
+                  />
+                  <label
+                    className="connectAccountCheckboxLabel"
+                    htmlFor={`account#${index}`}
+                  >
+                    {accountOption.accountId}
+                  </label>
+                </div>
+              ))
+            : connectAccountOptions
+                ?.filter((accountOption) => accountOption?.isChosen)
+                ?.map((accountOption, index) => (
+                  <div
+                    className="connectAccountCheckboxWrapper chosenAccount"
+                    key={index}
+                  >
+                    <div className="connectAccountCheckboxLabel">
+                      {accountOption.accountId}
+                    </div>
+                  </div>
+                ))}
+        </div>
+      </div>
+      <footer>
+        {tabIndex === 2 ? (
+          <div className="allowsMessage">
+            Allow this site to: See address, account balance, <br /> activity
+            and suggest transactions to approve
+          </div>
+        ) : null}
+        <div className="warningMessage">Only connect with sites you trust.</div>
         <button
           type="button"
           className="connectAccountsBtn confirm"
-          onClick={onConfirm}
-          disabled={isConfirming}
+          onClick={
+            tabIndex === 1 && isAnyAccountChosen
+              ? handleNext
+              : handleConnectAccounts
+          }
+          disabled={isConnecting}
         >
-          {isConfirming ? <ClipLoader color="#fff" size={14} /> : "Confirm"}
+          {isConnecting ? (
+            <ClipLoader color="#fff" size={14} />
+          ) : tabIndex === 1 && isAnyAccountChosen ? (
+            "Next"
+          ) : isAnyAccountChosen ? (
+            "Connect"
+          ) : (
+            "Confirm"
+          )}
         </button>
         <button
           type="button"
           className="connectAccountsBtn cancel"
           onClick={onCancel}
-          disabled={isConfirming}
+          disabled={isConnecting}
         >
           Cancel
         </button>
-      </div>
+      </footer>
     </div>
   );
 };
